2d74cf529d5a863c559c2cdb35eadbb2ed0ac871
2439905184 <suchenglong018@126.com>
2022-11-20 11:42:08 +0800

    🎈 perf: 完善程序
    做成死循环的，方便快速使用

--- main.nim
+++ main.nim
@@ -2,26 +2,41 @@ import std/strutils
 import std/strformat
 import std/math
 
-echo "--- 欢迎使用比例计算器 ---"
-echo "--- 请输入你要使用的功能"
-echo "0 计算等比乘法(整数模式)"
-echo "1 计算比值并转换成最近整数比"
-echo "2 计算等比乘法（小数模式）"
 
 proc 等比乘法_整数模式():void
 proc 等比乘法_小数模式():void
 proc 最简整数比():void
 
-var select:int = parseInt(stdin.readLine())
-if select == 0:
-  等比乘法_整数模式()
-elif select == 1:
-  最简整数比()
-elif select == 2:
-  等比乘法_小数模式()
-else:
-  echo "错误！不支持的选项"
+var dbcf_index = 0
+var zsb_index = 0
+
+proc main()  = 
+  # todo: 添加多种结果自动保存命名方式
+  echo "--- 欢迎使用比例计算器 ---"
+  echo "--- 请输入你要使用的功能"
+  echo "0 计算等比乘法(整数模式)"
+  echo "1 计算比值并转换成最近整数比"
+  echo "2 计算等比乘法（小数模式）"    
+  var select:int = parseInt(stdin.readLine())
   
+  if select == 0:
+    等比乘法_整数模式()
+  elif select == 1:
+    最简整数比()
+  elif select == 2:
+    等比乘法_小数模式()
+  else:
+    echo "错误！不支持的选项"
+    main()
+
+main()
+
+proc 等比乘法_索引增加() = 
+  dbcf_index += 1
+
+proc 最简整数比_索引增加() = 
+  zsb_index += 1
+
 # 计算最简整数比
 proc fx(p1,p2:int):array[2,int] = 
   var answer:array[2,int]
@@ -39,14 +54,17 @@ proc 等比乘法_整数模式() =
   var b1:int = 0
   echo "请输入循环次数"
   var times = parseInt(stdin.readLine())
-  var f:File = open("等比乘法结果.txt",fmWrite)
+  等比乘法_索引增加()
+  var f:File = open(fmt"等比乘法结果{dbcf_index}.txt",fmWrite)
   f.write(fmt"基础比例为: {a}:{b}" & "\n")
   for index in 1..times:
     a1 = a*index
     b1 = b*index
     f.write(fmt"{a1} {b1}" & "\n")
   f.close()
-  echo "运算已完成: 请查看: 等比乘法结果.txt"
+  echo fmt"运算已完成: 请查看: 等比乘法结果{dbcf_index}.txt"
+  main()
+
 
 proc 等比乘法_小数模式() = 
   echo "请输入比率如4.5 : 3.5 就先输入一个 4.5 回车 再输入 3.5 回车"
@@ -57,14 +75,16 @@ proc 等比乘法_小数模式() =
   var b1:float = 0
   echo "请输入循环次数"
   var times = parseInt(stdin.readLine())
-  var f:File = open("等比乘法结果.txt",fmWrite)
+  等比乘法_索引增加()
+  var f:File = open(fmt"等比乘法结果{dbcf_index}.txt",fmWrite)
   f.write(fmt"基础比例为: {a}:{b}" & "\n")
   for index in 1..times:
     a1 = a*float(index)
     b1 = b*float(index)
     f.write(fmt"{a1} {b1}" & "\n")
   f.close()
-  echo "运算已完成: 请查看: 等比乘法结果.txt"
+  echo fmt"运算已完成: 请查看: 等比乘法结果{dbcf_index}.txt"
+  main()
 
 
 # 利用格式输入语句将输入的两个数分别赋给 a 和 b，然后判断 a 和 b 的关系，如果 a 小于 b，则利用中间变量 t 将其互换。再利用辗转相除法求出最大公约数，进而求出最小公倍数。最后用格式输出语句将其输出。
@@ -73,7 +93,9 @@ proc 最简整数比() =
   var a:int = parseInt(stdin.readLine())
   var b:int = parseInt(stdin.readLine())
   var answer = fx(a,b)
-  var f:File = open("化简结果.txt",fmWrite)
+  最简整数比_索引增加()
+  var f:File = open(fmt"化简结果{zsb_index}.txt",fmWrite)
   f.write(fmt"输入为: {a}:{b}" & "\n")
   f.write(fmt"结果为: {answer[0]}:{answer[1]}")
-  echo "运算已完成: 请查看: 化简结果.txt"
+  echo fmt"运算已完成: 请查看: 化简结果{zsb_index}.txt"
+  main()
